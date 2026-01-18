2c8bc2bce5027ae233f84c3d65f0221e324462d0
fractal27 <giacomo.dominici07@gmail.com>
2026-01-18 01:03:28 +0100

    blocks.h

--- blocks.h
+++ blocks.h
@@ -4,11 +4,16 @@
 
 
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		                                /*Update Interval*/	/*Update Signal(SIGSEGV,ecc...)*/
-	{"\3 Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }'",	                30,		NO_SIGNAL},
-	// {"\4 Weather:", "weather",	                                                1000,   NO_SIGNAL},
-	{"\5 ", "date '+(%a) %H:%M:%S'",					                            1,		NO_SIGNAL},
-	{"\6 HDD:", "df -h /|tail -1|rev|cut -d ' ' -f2|rev",					    50,		SIGUSR1}
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal(SIGSEGV,ecc...)*/
+	{" Mem: \3 ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }'",						30,     NO_SIGNAL},
+	{" Eth: ",
+		"VAR=$(ip a | grep eno1\\: | awk '// {print $9;}');"
+		"if [ \"$VAR\" = \"UP\" ]; then"
+		"	printf '\\6 '$VAR;"
+		"else printf '\\7 '$VAR;"
+		"fi",																		2,		NO_SIGNAL},
+	{" Date: \5 ", "date '+%d (%a) %H:%M:%S'",										1,		NO_SIGNAL},
+	{" hdd: \4 ", "df -h /|tail -1|rev|cut -d ' ' -f2|rev",							50,		NO_SIGNAL}
 };
 
 // \3, \4 \5 are the colorschemes
